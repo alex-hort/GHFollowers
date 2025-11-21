d85ebde22851e4c0592e894a63ef97e9547de96a
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-11-21 15:17:18 -0600

    Initial Commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Alexis Horteales Espinosa on 21/11/25.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Alexis Horteales Espinosa on 21/11/25.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+       
+    }
+    
+
+
+}

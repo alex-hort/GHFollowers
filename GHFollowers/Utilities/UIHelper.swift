7b2f820edcbb7e3b79ce3bd39883f3d2ed21c429
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-11-26 16:02:04 -0600

    9.FlowLayout, DSource, ARC etc.

---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,30 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Alexis Horteales Espinosa on 26/11/25.
+//
+
+import UIKit
+
+struct UIHelper{
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewLayout{
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        flowLayout.minimumInteritemSpacing = minimumItemSpacing
+        flowLayout.minimumLineSpacing = minimumItemSpacing
+        
+        return flowLayout
+        
+    
+    }
+}
